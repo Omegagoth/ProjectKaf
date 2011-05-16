fb99d40c13feda4389d3706cb107b86c87cd84ca
manquat <manquat@45da95b8-0297-4318-9beb-123545045c4a>
2011-05-16 08:09:55 +0000

    jai oublier d'add les new fichier
    git-svn-id: https://svn.epfl.ch/svn/projetkaf@72 45da95b8-0297-4318-9beb-123545045c4a

---
+++ trunk/Courbe3D.h
@@ -0,0 +1,25 @@
+#ifndef COURBE3D_H
+#define COURBE3D_H
+
+#include "wxIncludes.h"
+#include "Courbe.h"
+
+class Courbe3D : public Courbe
+{
+	public :
+	
+		Courbe3D();
+		Courbe3D(Vecteur3D, Vecteur3D, double, double);
+		virtual ~Courbe3D();
+		
+		void dessine() const;
+		
+		virtual Vecteur3D getchamps_magnetique(Particule const&) const{}
+		virtual Courbe3D* copie() const{}
+		virtual void affiche(ostream&) const{}
+	
+	protected :
+	
+};
+
+#endif
